bff8ad29de14d163d68996deb4c5ea2bf76508f5
Ayush Salunkhe <ayush.salunkhe7371@gmail.com>
2025-07-23 14:40:03 +0000

    what the fuck are you doing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Gotham', 'sans-serif'],
+        headline: ['Gotham', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
